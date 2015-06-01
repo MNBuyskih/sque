c4368b87e2e097954c3cc5902d8f0b77047d6895
MNB <buyskih@gmail.com>
2015-06-01 16:13:21 +0300

    Реализована публикация задачи по запросу

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,26 +1,33 @@
-/*global describe, it, beforeEach*/
+/*global describe, it, beforeEach, afterEach*/
 "use strict";
 
 var assert = require('chai').assert,
     Manager = require('../lib/manager.js'),
     r = require('redis'),
     redis = r.createClient(),
-    fail = require('../');
+    fail = require('fail.js');
 
 describe('Manager', function () {
     var m = new Manager('test'),
+        c = r.createClient(),
         client;
 
     beforeEach(function (done) {
         redis.del('sq:test', function () {
-            client = r.createClient();
-            client.subscribe('sq:test:jobs', function (error) {
-                if (error) {
-                    done(error);
-                }
-                done();
-            });
+            m.push('a');
+            m.push('b');
+            m.push('c');
+            m.push('d');
+
+            m.push({
+                a: 0,
+                b: 1,
+                c: 2,
+                d: 3
+            }, done);
+
         });
+
     });
 
     describe('push', function () {
@@ -34,7 +41,7 @@ describe('Manager', function () {
                             done(error);
                         } else {
                             assert.lengthOf(result, 1);
-                            assert.equal(result[0], 'i am a new job');
+                            assert.equal(result[0], 'a');
                             done();
                         }
                     });
@@ -45,33 +52,29 @@ describe('Manager', function () {
 
     describe('publish', function () {
         it('should push message with new job', function (done) {
+            client = r.createClient();
+            client.subscribe('sq:test:jobs', fail(done, function () {
+                client.on('message', function (channel, data) {
+                    assert.equal(channel, 'sq:test:jobs');
+                    assert.equal(data, 'a');
+                    done();
+                });
+
+                m.publish();
+            }));
+        });
+    });
+
+    it('should listen for new clients', function (done) {
+        client = r.createClient();
+        client.subscribe('sq:test:jobs:customid', fail(done, function () {
             client.on('message', function (channel, data) {
-                assert.equal(channel, 'sq:test:jobs');
+                assert.equal(channel, 'sq:test:jobs:customid');
                 assert.equal(data, 'a');
                 done();
             });
 
-            m.push('a');
-            m.push('b');
-            m.push('c');
-            m.push('d');
-
-            m.push({
-                a: 0,
-                b: 1,
-                c: 2,
-                d: 3
-            }, function (error) {
-                if (error) {
-                    done(error);
-                } else {
-                    m.publish(function (error, clients) {
-                        if (error) {
-                            done(error);
-                        }
-                    });
-                }
-            });
-        });
+            c.publish(['sq:test:iamfree', 'customid'], fail.noop);
+        }));
     });
 });
